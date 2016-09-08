62a71b3653f550de8f3ad997476b8aea001c0777
Joe <txjoe75@gmail.com>
2016-09-08 13:14:48 -0500

    resume update

--- client/static/js/controllers/form.js
+++ client/static/js/controllers/form.js
@@ -7,15 +7,18 @@ $(document).ready(function(){
 		var email = $('[name=email]').val();
 		var subject = $('[name=subject]').val();
 		var message = $('[name=message]').val();
+		var resume = $('[name=resume').val();
 
 		$.ajax({
 			type: "POST",
 			url: "/php-bin/process_contact_form.php",
-			data: { name: name, email : email, subject: subject, message: message}
+			data: { name: name, email : email, subject: subject, message: message, resume: resume}
 		}).success(function(html){
 			form_holder_feedback = html;
+			$('#send')[0].reset();
 		}).error(function(html){
 			form_holder_feedback = '<h2 id="message_feedback"> There was an error sending your message.</h2>';
+			
 		});		
 	});
 });
