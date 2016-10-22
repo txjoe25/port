ce2f7b4c4bccc8c19307a6fe94f11cd20ce818c5
Joe <txjoe75@gmail.com>
2016-10-21 22:49:10 -0500

    update2

--- client/static/js/controllers/factoryController.js
+++ client/static/js/controllers/factoryController.js
@@ -1,17 +1,4 @@
-// // angular.module('ngRepeat', ['ngAnimate']).controller('factoryController', function($scope) {
-// //   $scope.friends = [
-// //     {name:'John', age:25, gender:'boy'},
-// //     {name:'Jessie', age:30, gender:'girl'},
-// //     {name:'Johanna', age:28, gender:'girl'},
-// //     {name:'Joy', age:15, gender:'girl'},
-// //     {name:'Mary', age:28, gender:'girl'},
-// //     {name:'Peter', age:95, gender:'boy'},
-// //     {name:'Sebastian', age:50, gender:'boy'},
-// //     {name:'Erika', age:27, gender:'girl'},
-// //     {name:'Patrick', age:40, gender:'boy'},
-// //     {name:'Samantha', age:60, gender:'girl'}
-// //   ];
-// // });
+
 // // 2. This code loads the IFrame Player API code asynchronously.
 //       var tag = document.createElement('script');
 
