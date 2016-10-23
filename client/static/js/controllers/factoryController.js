f741e9e575def8766ae92bdbd23b14c5c6fac814
Joe <txjoe75@gmail.com>
2016-10-22 22:06:50 -0500

    youtube & twitch

--- client/static/js/controllers/factoryController.js
+++ client/static/js/controllers/factoryController.js
@@ -13,7 +13,7 @@
 //         player = new YT.Player('player', {
 //           height: '690',
 //           width: '1280',
-//           videoId: 'M7lc1UVf-VE',
+//           videoId: 'zewtIQZHFkQ',
 //           events: {
 //             'onReady': onPlayerReady,
 //             'onStateChange': onPlayerStateChange
@@ -45,7 +45,7 @@
 //   player = new YT.Player('player', {
 //     height: '390',
 //     width: '640',
-//     videoId: 'M7lc1UVf-VE',
+//     videoId: 'zewtIQZHFkQ',
 //     events: {
 //       'onReady': onPlayerReady,
 //       'onStateChange': onPlayerStateChange
--- client/static/js/controllers/form.js
+++ client/static/js/controllers/form.js
@@ -1,48 +1,46 @@
 $(document).ready(function(){
 	var form_holder = $('#form_holder');
 	var form_holder_feedback = '';
-	console.log("doc.ready")
+	console.log("doc.ready");
 	$(document).on('click','#Joe', function(){
         var div = $("#Joe");
-        var rap = $("#player");
-
+        var home = $("#home");
+        var ball = $("#ball")
         div.animate({height: '5em', opacity: '0.4'}, "slow");
         div.animate({width: '-5em', opacity: '0.8'}, "slow");
         div.animate({height: '10em', opacity: '0.4'}, "slow");
         div.animate({width: '-10em', opacity: '0.8'}, "slow");
         div.animate({width: '10em', opacity: '0.8'}, "slow");
         div.animate({height: '10em', opacity: '1'}, "slow");
-        
-        rap.show();
-        rap.animate({width: '-5em', opacity: '0.8'}, "slow");
-        rap.animate({height: '-5em', opacity: '0.8'}, "slow");
 
-		rap.animate({height: '10em', opacity: '0.4'}, "slow");
-  		rap.animate({width: '10em', opacity: '0.8'}, "slow");
-  		rap.rotate({ 
-		   bind: 
-		     { 
-		        mouseover : function() { 
-		            $(this).rotate({animateTo:180})
-		        },
-		        mouseout : function() { 
-		            $(this).rotate({animateTo:0})
-		        }
-		     } 
-		   
+  		ball.show();
+  		$("#youtube").show();
+  		$("#twitch").show();
+  	});
+  		$(document).on('click','#twitch',function(){
+  			var name = $("#name");
+        	var joe = $("#Joe_Agnew")
+  			var twitch = $("#player1");
+  				$('#home').hide();
+  				twitch.show();
+  			var options = {
+				width: 1500,
+				height: 3000,
+				channel: "txjoe25",
+				//video: "{VIDEO_ID}"
+				};
+
+			var player = new Twitch.Player("{PLAYER_DIV_ID}", options);
+			player.setVolume(0.5);
+
 		});
-    });
-
-    $(document).on('click','#Joe_Agnew',function(){
-	  $( "#Joe" ).css({
-	    width: "",
-	    height: "",
-	    opacity: ""
-
-	  });
-	
-	  $("#player").hide("Fast")
-    })
+		$(document).on('click','#ball',function(){
+			if('#player1' || '#player'){
+				$('#player1').hide();
+				$('#player').hide();
+				$('#home').show();
+			}
+		});  
 
 	$(document).on('click', '#send', function(){
 		var name= $('[name=name]').val();
