78762b899a3374988bb7e5643a623decadae80c5
Joe <txjoe75@gmail.com>
2016-11-05 19:40:21 -0500

    php

--- php-bin/process_contact_form.php
+++ php-bin/process_contact_form.php
@@ -1,5 +1,5 @@
 <?php
-	if (isset($_POST['name'], $_POST['email'], $_POST['message'],)){
+	if (isset($_POST['name'], $_POST['email'], $_POST['message'], $_POST['resume'])){
 		$to = 'txjoe25@gmail.com';
 		$subject = 'Message from ' . $_POST['name'] . ' (' . $_POST['email'] . ')';
 		$message = $_POST['message'];
